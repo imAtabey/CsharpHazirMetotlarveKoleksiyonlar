c255550b11a7fb20f7f4cfccaf5e9a6084daacac
Hüseyin ÜTEBAY <huseyin.utebay@gmail.com>
2025-08-02 15:21:22 +0300

    Datetime ve Math Sınıf Metotlarıı Eklendi

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿string degisken = "Dersimiz Csharp,hoşgeldiniz";
+﻿#region String Metotları
+
+string degisken = "Dersimiz Csharp,hoşgeldiniz";
 string degisken2 = "Dersimiz Csharo";
 
 // length -> uzunluğu verir
@@ -52,3 +54,66 @@ Console.WriteLine(degisken.Split(' ')[1]); //Boşluk ile ayır index 1'i getir
 // SubString ->  İlgili indexten başlayıp sonuna kadar veya istenilen sayıda karakteri getirir
 Console.WriteLine(degisken.Substring(4));
 Console.WriteLine(degisken.Substring(4, 2));
+#endregion
+
+#region DateTime ve Math Metotları
+// DateTime ->
+Console.WriteLine(DateTime.Now);
+Console.WriteLine(DateTime.Now.Date);
+Console.WriteLine(DateTime.Now.Day);
+Console.WriteLine(DateTime.Now.Month);
+Console.WriteLine(DateTime.Now.Year);
+Console.WriteLine(DateTime.Now.Hour);
+Console.WriteLine(DateTime.Now.Minute);
+Console.WriteLine(DateTime.Now.Second);
+
+Console.WriteLine(DateTime.Now.DayOfWeek); //haftanın günü string
+Console.WriteLine(DateTime.Now.DayOfYear); //yılın günü
+
+Console.WriteLine(DateTime.Now.ToLongDateString());
+Console.WriteLine(DateTime.Now.ToShortDateString());
+Console.WriteLine(DateTime.Now.ToLongTimeString());
+Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+Console.WriteLine(DateTime.Now.AddDays(2));
+Console.WriteLine(DateTime.Now.AddHours(2));
+Console.WriteLine(DateTime.Now.AddSeconds(30));
+Console.WriteLine(DateTime.Now.AddMonths(5));
+Console.WriteLine(DateTime.Now.AddYears(10));
+Console.WriteLine(DateTime.Now.AddMilliseconds(50));
+
+//DateTime Format ->
+Console.WriteLine(DateTime.Now.ToString("dd")); //gün
+Console.WriteLine(DateTime.Now.ToString("ddd")); //sat
+Console.WriteLine(DateTime.Now.ToString("dddd")); //saturday
+
+Console.WriteLine(DateTime.Now.ToString("MM")); //04
+Console.WriteLine(DateTime.Now.ToString("MMM")); //Apr
+Console.WriteLine(DateTime.Now.ToString("MMMM")); //April
+
+Console.WriteLine(DateTime.Now.ToString("yy")); //21
+Console.WriteLine(DateTime.Now.ToString("yyy")); //2021
+
+Console.WriteLine("*** Math Kütüphanesi ***");
+
+// Math Kütüphanesi ->
+Console.WriteLine(Math.Abs(-1)); // Mutlak alır
+Console.WriteLine(Math.Sin(30)); // sin
+Console.WriteLine(Math.Cos(30)); // cos
+Console.WriteLine(Math.Tan(30)); // Tan
+
+Console.WriteLine(Math.Ceiling(22.3)); // 23 bir üst tam
+Console.WriteLine(Math.Round(22.3)); // 23 en yakına yuvarlar
+Console.WriteLine(Math.Floor(22.3)); // 22 bir alt tam
+
+Console.WriteLine(Math.Max(6, 2)); //Max değer
+Console.WriteLine(Math.Min(6, 2)); //Min değer
+
+Console.WriteLine(Math.Pow(3, 4)); //3^4 = 81
+Console.WriteLine(Math.Sqrt(9)); //karekök = 3
+Console.WriteLine(Math.Log(9)); // 9'un e tabanında karşılığı
+Console.WriteLine(Math.Exp(3)); // e^3 verir
+Console.WriteLine(Math.Log10(10)); // 10'un log10 da karşılığı
+
+
+#endregion
--- obj/Debug/net9.0/StringMetotlar.AssemblyInfo.cs
+++ obj/Debug/net9.0/StringMetotlar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringMetotlar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88f0e1c3b442d223cc14f588542559847c0b3ec3")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringMetotlar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringMetotlar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
